c77de70f736efd096c1e467585b7347b0ca65a99
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-20 11:48:03 -0600

    Add empty Xcode project

---
+++ Sleepsta/SleepstaTests/SleepstaTests.swift
@@ -0,0 +1,34 @@
+//
+//  SleepstaTests.swift
+//  SleepstaTests
+//
+//  Created by Dillon McElhinney on 3/20/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import XCTest
+@testable import Sleepsta
+
+class SleepstaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
