36354ef092b417d1f9f5ade131fb1f617996f0ee
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-20 15:01:08 -0600

    Lay out Login/Signup view
    Also add some helper files to make styling the app a little easier

---
+++ Sleepsta/Sleepsta/LoginManager.swift
@@ -0,0 +1,23 @@
+//
+//  LoginManager.swift
+//  Sleepsta
+//
+//  Created by Dillon McElhinney on 3/20/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
+
+class LoginManager {
+    
+    private let baseURL = URL(string: "")!
+    
+    func loginUser(username: String, password: String, completion: @escaping () -> Void) {
+        
+    }
+    
+    func registerUser(username: String, password: String,  completion: @escaping () -> Void) {
+        
+    }
+    
+}
