b13f144c64f9fe8ca51e54c1d213c00975bfc7a0
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-28 15:29:02 -0600

    Add date label and notes to stats view

--- Sleepsta/Sleepsta/StatsViewController.swift
+++ Sleepsta/Sleepsta/StatsViewController.swift
@@ -12,7 +12,10 @@ import Charts
 class StatsViewController: UIViewController {
     var dailyData: DailyData?
 
+    @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var lineChart: LineChartView!
+    @IBOutlet weak var notesLabel: UILabel!
+    @IBOutlet weak var notesTextView: UITextView!
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -20,13 +23,15 @@ class StatsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let gradientView = view as! GradientView
-        gradientView.setupGradient(startColor: .darkerBackgroundColor, endColor: .lighterBackgroundColor)
         
-        loadSampleData()
-        setupChart()
-        updateChart()
+        setupViews()
+        
+        // If there is no data, load the sample data
+        if dailyData == nil { loadSampleData() }
+        
+        // Update charts and labels
+        updateLineChart()
+        updateLabels()
     }
 
     private func loadSampleData() {
@@ -35,20 +40,28 @@ class StatsViewController: UIViewController {
         dailyData = try! JSONDecoder().decode(DailyData.self, from: data)
     }
     
-    private func setupChart() {
-        lineChart.xAxis.labelTextColor = .customWhite
-        lineChart.xAxis.gridColor = .darkBlue
-        lineChart.xAxis.labelFont = UIFont.preferredFont(forTextStyle: .caption1)
-        lineChart.xAxis.labelRotationAngle = 45
-        lineChart.xAxis.labelPosition = .bottom
-        lineChart.xAxis.valueFormatter = TimeXAxisValueFormatter()
+    private func setupViews() {
         
+        // Set up gradient background
+        let gradientView = view as! GradientView
+        gradientView.setupGradient(startColor: .darkerBackgroundColor, endColor: .lighterBackgroundColor)
+        
+        // Set up date label
+        dateLabel.textColor = .customWhite
+        
+        // Set up line chart
         let leftAxis = lineChart.getAxis(.left)
         let rightAxis = lineChart.getAxis(.right)
         rightAxis.drawLabelsEnabled = false
         leftAxis.drawLabelsEnabled = false
         leftAxis.gridColor = .darkBlue
         rightAxis.gridColor = .darkBlue
+        lineChart.xAxis.labelTextColor = .customWhite
+        lineChart.xAxis.gridColor = .darkBlue
+        lineChart.xAxis.labelFont = UIFont.preferredFont(forTextStyle: .caption1)
+        lineChart.xAxis.labelRotationAngle = 45
+        lineChart.xAxis.labelPosition = .bottom
+        lineChart.xAxis.valueFormatter = TimeXAxisValueFormatter()
         lineChart.legend.enabled = false
         lineChart.drawGridBackgroundEnabled = false
         lineChart.backgroundColor = .clear
@@ -60,13 +73,31 @@ class StatsViewController: UIViewController {
         lineChart.scaleYEnabled = false
         lineChart.drawBordersEnabled = true
         lineChart.borderColor = .darkBlue
-        
         lineChart.noDataText = "No motion data for this day"
         lineChart.noDataTextColor = .white
         lineChart.noDataFont = UIFont.preferredFont(forTextStyle: .headline)
+        
+        // Set up notes label
+        notesLabel.textColor = .customWhite
+        
+        // Set up notes text view
+        notesTextView.textColor = .customWhite
+        notesTextView.backgroundColor = .clear
     }
     
-    private func updateChart() {
+    private func updateLabels() {
+        guard isViewLoaded, let dailyData = dailyData else { return }
+        
+        dateLabel.text = dailyData.dateRangeString
+        
+        if let notes = dailyData.sleepNotes, !notes.isEmpty {
+            notesTextView.text = notes
+        } else {
+            notesLabel.text = "No notes."
+        }
+    }
+    
+    private func updateLineChart() {
         guard let motionData = dailyData?.motionData, !motionData.isEmpty else { return }
         var entries: [ChartDataEntry] = []
         for dataPoint in motionData {
--- Sleepsta/Sleepsta/UI Styling/UIColor+CustomColors.swift
+++ Sleepsta/Sleepsta/UI Styling/UIColor+CustomColors.swift
@@ -14,10 +14,12 @@ extension UIColor {
     static let lighterBackgroundColor = UIColor(hexRed: 37, green: 46, blue: 79)
     static let darkBlue = UIColor(hexRed: 76, green: 84, blue: 111)
     static let customWhite = UIColor(hexRed: 247, green: 247, blue: 255)
-    static let accentColor = UIColor(hexRed: 158, green: 228, blue: 147)
+    static let lightGreen = UIColor(hexRed: 158, green: 228, blue: 147)
     static let pink = UIColor(hexRed: 227, green: 74, blue: 111)
     static let mintGreen = UIColor(hexRed: 154, green: 210, blue: 203)
     static let systemBlue = UIColor(hexRed: 0, green: 122, blue: 255)
+    
+    static let accentColor = UIColor.lightGreen
     
     /// Convenience initializer for making UIColors from HEX values
     convenience init(hexRed: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) {
