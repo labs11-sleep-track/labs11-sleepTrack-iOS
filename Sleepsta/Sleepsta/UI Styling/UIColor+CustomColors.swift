d2ac8a6bb8a4faebb15584922848329919026465
Dillon McElhinney <dillon.mce@gmail.com>
2019-04-09 10:06:44 -0600

    Add initializer for making UIColor from HEX string

--- Sleepsta/Sleepsta/UI Styling/UIColor+CustomColors.swift
+++ Sleepsta/Sleepsta/UI Styling/UIColor+CustomColors.swift
@@ -22,9 +22,31 @@ extension UIColor {
     
     static let accentColor = UIColor.lightGreen
     
-    /// Convenience initializer for making UIColors from HEX values
+    /// Convenience initializer that returns a UIColor from HEX values
     convenience init(hexRed: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) {
         self.init(red: hexRed/255, green: green/255, blue: blue/255, alpha: alpha)
     }
     
+    /// Convenience initializer that returns a UIColor from a HEX string
+    convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        // Make a character set that will strip whitespace and #'s
+        var characterSet = CharacterSet.whitespacesAndNewlines
+        characterSet.insert(charactersIn: "#")
+        // Use it to format the hex string
+        let cString = hex.components(separatedBy: characterSet).joined().uppercased()
+        
+        // For now, if it is not exactly 6 characters, return nil
+        // TODO: Update to take a 3 character hex code
+        if (cString.count != 6) { return nil }
+        
+        // Scan the hex number into an Int
+        var rgbValue: UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        // Do some fancy hex math and initialize a UIColor
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8)
+        let blue = CGFloat(rgbValue & 0x0000FF)
+        self.init(hexRed: red, green: green, blue: blue, alpha: alpha)
+    }
 }
